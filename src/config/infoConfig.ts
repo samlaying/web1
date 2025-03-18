5c3df44fa18da2f6e4e0f2020f7bd13716f261e0
Samlaying <15351480+samlaying@user.noreply.gitee.com>
2025-03-18 12:38:29 +0800

    先传一版，还没修改

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,8 +3,8 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = '林承列'
+export const headline = '就读于天津师范大学经济学.'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
 export const email = 'tong.hu@scls-sh.org'
